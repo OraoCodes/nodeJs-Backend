bb582b8204f42fc9e44ac9cacd9b7f993f0d0518
oraowycliffe@students.uonbi.ac.ke <oraowycliffe@students.uonbi.ac.ke>
2022-07-31 14:25:53 +0300

    Call http Object, Create Server and listen to port 3000(devt purposes

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-const username = 'Orao123';
+//launch a http
+//create a server
+//listen in on the server
 
-console.log(username);
+const http = require("http");
+
+const server = http.createServer();
+
+server.listen("3000");
