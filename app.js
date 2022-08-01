9a45334097f4bfb549bb446f5176ef627ddb702f
oraowycliffe@students.uonbi.ac.ke <oraowycliffe@students.uonbi.ac.ke>
2022-08-01 13:01:44 +0300

    Parsing data to with express JS

--- app.js
+++ app.js
@@ -2,12 +2,20 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.urlencoded({extended: false}));
+
 app.get("/currenttime", function (request, response) {
   response.send("<h1>" + new Date().toISOString() + "</h1>");
 });
 
 app.get("/", function (req, res) {
-  res.send("<h3>Howdy Express</h3>");
+  res.send('<form action="/store-user" method="POST"><label>Your name .</label><br><input type="text" name="username"></input><br><button>Submit</button></form>');
+});
+
+app.post('/store-user', function(req, res){
+   const userName =  req.body.username;
+   console.log(userName);
+   res.send('<h2>Username Stored</h2>');
 });
 
 app.listen(3000);
